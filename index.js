3399d823fd9aa9820a397019d95e8aa02ac8b255
roriobarnoh <roriokiptoo2016@gmail.com>
2025-02-26 14:48:45 +0300

    arithmetics

---
+++ index.js
@@ -0,0 +1,32 @@
+function add(a, b){
+    return a+ b;
+}
+add(900, 9);
+function subtract(a, b){
+ return a-b;
+}
+subtract(580, 5)
+function multiply(a, b){
+    return a*b;
+}
+subtract(734597, 1);
+function divide(a, b){
+    return a/b; 
+}
+divide(15/0.12);
+function increment(n){
+    return n+1;
+}
+increment(10);
+function decrement(n){
+    return n-1;
+}
+decrement(9);
+function makeInt(n){
+    return parseInt(n, 10);
+}
+makeInt("2", 10);
+function preserveDecimal(n){
+    return parseFloat(n);
+}
+preserveDecimal("80.123999");
